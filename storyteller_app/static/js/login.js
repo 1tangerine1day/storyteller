d32c871d97a59c14c6eefedf39638c3c53cb1caa
Dan Haur <jack8409201@gmail.com>
2016-12-19 09:39:32 +0000

    fix

--- storyteller_app/static/js/login.js
+++ storyteller_app/static/js/login.js
@@ -2,60 +2,61 @@
 ---------------------------------------
 */
 $(document).ready(function() {
+  $("#log-in").click(function () {
+    FB.getLoginStatus(function(response){
+      if(response.status === 'connected'){
+        location.href= ("https://storyteller-test-cloned-a1tangerine1day.c9users.io/"); 
+      }
+      else{
+        $("#log_in_page").show();
+      }
+    });  
+  });
 
-
-
-
-$("#log-in").click(function () {
-  $("#log_in_page").show();
-});
-
-$("#close_log").click(function () {
-  $("#log_in_page").hide();
-});
-
-
-
+  $("#close_log").click(function () {
+    $("#log_in_page").hide();
+  });
 });
 
 
 function cambiar_login() {
   document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";  
-document.querySelector('.cont_form_login').style.display = "block";
-document.querySelector('.cont_form_sign_up').style.opacity = "0";               
+  document.querySelector('.cont_form_login').style.display = "block";
+  document.querySelector('.cont_form_sign_up').style.opacity = "0";               
 
-setTimeout(function(){  document.querySelector('.cont_form_login').style.opacity = "1"; },400);  
+  setTimeout(function(){  
+    document.querySelector('.cont_form_login').style.opacity = "1"; 
+    
+  },400);  
   
-setTimeout(function(){    
-document.querySelector('.cont_form_sign_up').style.display = "none";
-},200);  
-  }
+  setTimeout(function(){    
+    document.querySelector('.cont_form_sign_up').style.display = "none";
+  },200);  
+}
 
 function cambiar_sign_up(at) {
   document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
   document.querySelector('.cont_form_sign_up').style.display = "block";
-document.querySelector('.cont_form_login').style.opacity = "0";
+  document.querySelector('.cont_form_login').style.opacity = "0";
   
-setTimeout(function(){  document.querySelector('.cont_form_sign_up').style.opacity = "1";
-},100);  
+  setTimeout(function(){  
+    document.querySelector('.cont_form_sign_up').style.opacity = "1";
+  },100);  
 
-setTimeout(function(){   document.querySelector('.cont_form_login').style.display = "none";
-},400);  
-
-
+  setTimeout(function(){   
+    document.querySelector('.cont_form_login').style.display = "none";
+  },400);  
 }    
 
-
-
 function ocultar_login_sign_up() {
+  document.querySelector('.cont_forms').className = "cont_forms";  
+  document.querySelector('.cont_form_sign_up').style.opacity = "0";               
+  document.querySelector('.cont_form_login').style.opacity = "0"; 
 
-document.querySelector('.cont_forms').className = "cont_forms";  
-document.querySelector('.cont_form_sign_up').style.opacity = "0";               
-document.querySelector('.cont_form_login').style.opacity = "0"; 
+  setTimeout(function(){
+    document.querySelector('.cont_form_sign_up').style.display = "none";
+    document.querySelector('.cont_form_login').style.display = "none";
+  },500);  
+}
 
-setTimeout(function(){
-document.querySelector('.cont_form_sign_up').style.display = "none";
-document.querySelector('.cont_form_login').style.display = "none";
-},500);  
-  
-  }
+
