c82698cb5687c493992778bb4212f4b6481bfdb6
Jerry <blawhi2435@gmail.com>
2016-12-22 14:27:08 +0800

    modify navbar

--- storyteller_app/views.py
+++ storyteller_app/views.py
@@ -92,8 +92,9 @@ def collection_e(request):
     paginator = Paginator(contact_list, 9) # Show 25 contacts per page
 
     page = request.GET.get('page')
+    
     try:
-        contacts = paginator.page(page)
+        contacts = paginator.page(page)    
     except PageNotAnInteger:
         # If page is not an integer, deliver first page.
         contacts = paginator.page(1)
@@ -102,13 +103,6 @@ def collection_e(request):
         contacts = paginator.page(paginator.num_pages)
 
     return render(request, 'storyteller_app/collection_e.html', {'contacts': contacts})
-    
-  
-  
-  
- 
-  
-  
     
 def collection_f(request):
     
@@ -183,13 +177,22 @@ def index(request):
 def post_likes(request, pk):
     
     temp_pk = pk
-    
     Post.objects.filter(pk=temp_pk).update(post_likes = F('post_likes')+1)
+    contact_list = Post.objects.all()
+    paginator = Paginator(contact_list, 9) # Show 25 contacts per page
+    page = request.GET.get('page')
+
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first pag.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        contacts = paginator.page(paginator.num_pages)
+
+    return render(request, 'storyteller_app/collection_e.html', {'contacts': contacts})
     
-    post_list  = Post.objects.all()
-    
-    
-    return render(request, 'storyteller_app/collection_e.html',{'post_list': post_list })
     
    
    
